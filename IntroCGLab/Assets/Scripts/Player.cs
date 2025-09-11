10062d93bd80c3365958cad28a3889a9838595d1
Bilal Khan <115832016+BillsKDev@users.noreply.github.com>
2025-09-11 19:02:45 -0400

    submit

---
+++ IntroCGLab/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float speed = 5f;
+    Vector2 moveInput;
+    
+    Controls controls;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        controls = new Controls();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnEnable() => controls.Enable();
+    void OnDisable() => controls.Disable();
+
+    void Update()
+    {
+        Move();
+    }
+    
+    void Move()
+    {
+        moveInput = controls.Player.Move.ReadValue<Vector2>();
+        Vector3 movement = (transform.right * moveInput.x + transform.forward * moveInput.y).normalized;
+        rb.MovePosition(movement * (speed * Time.deltaTime));
+    }
+}
